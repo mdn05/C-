fb1e5ca57bffb9499bfdfb9986de65770fb6a896
DN <molch.d@yandex.ru>
2023-03-10 12:51:36 +0300

    dobavlenie reshenia zadach :wdobavlenie reshenia zadach :wq

--- lesson 1/Program.cs
+++ lesson 1/Program.cs
@@ -4,23 +4,23 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-//Console.WriteLine("Введи свое первое число - ");
-//int a = int.Parse(Console.ReadLine());
-//Console.WriteLine("Введи свое второе число - ");
-//int b = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введи свое первое число - ");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введи свое второе число - ");
+// int b = int.Parse(Console.ReadLine());
 
-//bool A = a > b;
+// bool A = a > b;
 
-//bool B = a < b;
+// bool B = a < b;
 
-//if (A)
-//{
-    //Console.WriteLine($"{a} больше чем {b}");
-//}
-//else if (B)
-//{
-    //Console.WriteLine($"{a} меньше чем {b}");
-//}
+// if (A)
+// {
+//     Console.WriteLine($"{a} больше чем {b}");
+// }
+// else if (B)
+// {
+//     Console.WriteLine($"{a} меньше чем {b}");
+// }
 
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и 
@@ -28,26 +28,16 @@
 //2, 3, 7 -> 7
 //44 5 78 -> 78
 //22 3 9 -> 22
-Console.WriteLine("введите 1 число -");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("введите 2 число -");
-int b = int.Parse(Console.ReadLine());
-Console.WriteLine("введите 3 число -");
-int c = int.Parse(Console.ReadLine());
-
-bool A = a > b; B = a > c; C = b > c;
-{if (A)
-{
-Console.WriteLine($"{a} больше чем {b}");
-}
-else if (B)
-{
-Console.WriteLine($"{a} больше чем {c}");
-}
-if (C)
-{   
-Console.WriteLine($"{b} больше чем {c}");          
-} }
+// Console.WriteLine("введите 1 число -");
+// int a1 = int.Parse(Console.ReadLine());
+// Console.WriteLine("введите 2 число -");
+// int b1 = int.Parse(Console.ReadLine());
+// Console.WriteLine("введите 3 число -");
+// int c1 = int.Parse(Console.ReadLine());
+// int max = a1;
+// if (b1 > max) max = b1;
+// if (c1 > max) max = c1;
+// Console.WriteLine(max);
 
 
 //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
@@ -57,8 +47,30 @@ Console.WriteLine($"{b} больше чем {c}");
 //-3 -> нет
 //7 -> нет
 
+// Console.WriteLine("введите число -");
+// int i = int.Parse(Console.ReadLine());
+// if (i % 2 == 0)
+// {
+//     Console.WriteLine("введенное число четное");
+//     Console.ReadLine();
+// }
+// else
+// {
+//     Console.WriteLine("введенное число нечетное");
+//     Console.ReadLine();
+// }
+
+
 //Задача 8: Напишите программу, которая на вход принимает число (N), 
 //          а на выходе показывает все чётные числа от 1 до N.
 
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
+
+//Console.WriteLine("Введите число -");
+//int n = Convert.ToInt32(Console.ReadLine());
+//for (int i=1;i<=n;i++)
+//{
+//if (i % 2 == 0)
+//System.Console.Write(i + " ,");
+//}
